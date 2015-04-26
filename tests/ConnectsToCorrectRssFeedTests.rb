dfac46917bf6835c61862afb8078e72ef7a7020e
iainjmitchell <iainjmitchell@gmail.com>
2015-04-26 17:32:10 +0100

    refactoring

--- tests/ConnectsToCorrectRssFeedTests.rb
+++ tests/ConnectsToCorrectRssFeedTests.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
 require 'rspec/expectations'
+require_relative '../lib/AwsStatus'
 
 class ConnectToCorrectRssFeedTests < Test::Unit::TestCase
 	include RSpec::Matchers
@@ -108,86 +109,3 @@ class ConnectToCorrectRssFeedTests < Test::Unit::TestCase
 	end
 end
 
-class AwsStatus
-	attr_reader :status
-
-	def initialize(rest)
-		@rest = rest
-		@status = DataCenters.new(rest)
-	end
-end
-
-class DataCenters 
-	DATA_CENTERS = {
-		:eu_west_1 => 'eu-west-1',
-		:eu_central_1 => 'eu-central-1',
-		:us_east_1 => 'us-east-1',
-		:us_west_1 => 'us-west-1',
-		:us_west_2 => 'us-west-2',
-		:sa_east_1 => 'sa-east-1',
-		:ap_northeast_1 => 'ap-northeast-1',
-		:ap_southeast_1 => 'ap-southeast-1',
-		:ap_southeast_2 => 'ap-southeast-2'
-	}
-
-	def initialize(rest_client)
-		@rest_client = rest_client
-	end
-
-	DATA_CENTERS.each do |data_center_name, rss_name|
-		define_method data_center_name do
-			DataCenter.new(@rest_client, rss_name)
-		end
-	end
-end
-
-class DataCenter
-	AWS_STATUS_RSS_ROOT = 'http://status.aws.amazon.com/rss'
-	SERVICES = {	
-		:cloudsearch => 'cloudsearch',
-		:cloudwatch => 'cloudwatch',
-		:dynamodb => 'dynamodb',
-		:elastic_compute_cloud => 'ec2',
-		:elastic_load_balancer => 'elb',
-		:elastic_map_reduce => 'emr',
-		:elasticache => 'elasticache',
-		:glacier => 'glacier',
-		:kinesis => 'kinesis',
-		:redshift => 'redshift',
-		:relational_database_service => 'rds',
-		:simple_email_service => 'ses',
-		:simple_queue_service => 'sqs',
-		:simple_notification_service => 'sns',
-		:simple_storage_service => 's3',
-		:simple_workflow_service => 'swf',
-		:simpledb => 'simpledb',
-		:virtual_private_cloud =>'vpc',
-		:auto_scaling => 'autoscaling',
-		:cloudformation => 'cloudformation',
-		:cloudhsm => 'cloudhsm',
-		:cloudtrail => 'cloudtrail',
-		:config => 'config',
-		:direct_connect => 'directconnect',
-		:elastic_beanstalk => 'elasticbeanstalk',
-		:identity_and_access => 'iam',
-		:key_management_service => 'kms',
-		:storage_gateway => 'storagegateway'
-	}
-
-	def initialize(rest_client, location)
-		@location = location
-		@rest_client = rest_client
-	end
-
-	SERVICES.each do |service_name, rss_name|
-		define_method service_name do
-			status_for rss_name
-		end
-	end
-
-	private
-
-	def status_for(component)
-		@rest_client.get("#{AWS_STATUS_RSS_ROOT}/#{component}-#{@location}.rss")
-	end
-end
